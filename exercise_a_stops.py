044bb228701c32456f76ababc4da646411193192
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-09-09 14:32:48 +0100

    exercise_a complete

--- exercise_a_stops.py
+++ exercise_a_stops.py
@@ -18,10 +18,40 @@ print(stops.index("Linlithgow"))
 
 #5. Remove "Livingston" from the list using its name
 
+print(stops)
+stops.remove('Livingston')
+print(stops)
 
 
 #6. Delete "Cumbernauld" from the list by index
+
+print(stops)
+
+stops.pop(2)
+
+print(stops)
+
+
 #7. Print the number of stops there are in the list
+
+print(len(stops))
+
+
 #8. Sort the list alphabetically
+
+stops.sort()
+
+print(stops)
+
 #9. Reverse the positions of the stops in the list
+
+stops.reverse()
+
+print(stops)
+
+
 #10 Print out all the stops using a for loop
+
+for stop in stops:
+    print(stop)
+    
