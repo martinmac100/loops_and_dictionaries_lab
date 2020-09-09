bc26b99cf87bcc405c8436ebd5a7cc0c4cf22904
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-09-09 15:27:32 +0100

    1st lab complete with addit Zsolt solutions

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -71,7 +71,20 @@ print(users['Erik']['lottery_numbers'])
 print(users['Avril']['pets'][0]['species'])
 
 # 5. Get the smallest of Erik's lottery numbers
+
+
+
 # 6. Return an array of Avril's lottery numbers that are even
+
+avrils_lottery_numbers = users["Avril"]["lottery_numbers"]
+even_numbers = []
+for number in avrils_lottery_numbers:
+  if number % 2 == 0:
+    even_numbers.append(number)
+
+print(even_numbers)
+
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "Fluffy"
